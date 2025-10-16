1c5226f3f409fdb27f3f1e7da950d491e2457e3a
Kyungsoo Lee <kyungsoo.lee@datahub.com>
2025-10-16 09:57:22 -0700

    fix(ingest): Remove unused import

--- metadata-ingestion/tests/unit/sql_parsing/test_sql_aggregator.py
+++ metadata-ingestion/tests/unit/sql_parsing/test_sql_aggregator.py
@@ -11,7 +11,6 @@ from datahub.configuration.datetimes import parse_user_datetime
 from datahub.configuration.time_window_config import BucketDuration, get_time_bucket
 from datahub.ingestion.sink.file import write_metadata_file
 from datahub.ingestion.source.usage.usage_common import BaseUsageConfig
-from datahub.metadata import schema_classes as models
 from datahub.metadata.urns import CorpUserUrn, DatasetUrn
 from datahub.sql_parsing.sql_parsing_aggregator import (
     KnownQueryLineageInfo,
